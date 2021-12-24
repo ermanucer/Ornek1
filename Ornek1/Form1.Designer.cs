79207b715d8653bf622ad44458f31b39b9c1b061
ermanucer <ermanucer@gmail.com>
2021-12-24 17:11:32 +0300

    Uygulama

--- Ornek1/Form1.Designer.cs
+++ Ornek1/Form1.Designer.cs
@@ -29,110 +29,59 @@ namespace Ornek1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.lblYas = new System.Windows.Forms.Label();
             this.lblYasananGun = new System.Windows.Forms.Label();
             this.lblBurc = new System.Windows.Forms.Label();
-            this.lblYasHesap = new System.Windows.Forms.Label();
-            this.lblYasananGunHesap = new System.Windows.Forms.Label();
-            this.lblBurcHesap = new System.Windows.Forms.Label();
             this.btnHesapla = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dtpDogumTarihi
             // 
-            this.dtpDogumTarihi.Location = new System.Drawing.Point(259, 47);
+            resources.ApplyResources(this.dtpDogumTarihi, "dtpDogumTarihi");
             this.dtpDogumTarihi.Name = "dtpDogumTarihi";
-            this.dtpDogumTarihi.Size = new System.Drawing.Size(200, 20);
-            this.dtpDogumTarihi.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(328, 9);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Doğum Tarihiniz";
             // 
             // lblYas
             // 
-            this.lblYas.AutoSize = true;
-            this.lblYas.Location = new System.Drawing.Point(310, 141);
+            resources.ApplyResources(this.lblYas, "lblYas");
             this.lblYas.Name = "lblYas";
-            this.lblYas.Size = new System.Drawing.Size(46, 13);
-            this.lblYas.TabIndex = 2;
-            this.lblYas.Text = "Yaşınız :";
             // 
             // lblYasananGun
             // 
-            this.lblYasananGun.AutoSize = true;
-            this.lblYasananGun.Location = new System.Drawing.Point(267, 164);
+            resources.ApplyResources(this.lblYasananGun, "lblYasananGun");
             this.lblYasananGun.Name = "lblYasananGun";
-            this.lblYasananGun.Size = new System.Drawing.Size(89, 13);
-            this.lblYasananGun.TabIndex = 2;
-            this.lblYasananGun.Text = "Yaşadığınız Gün :";
             // 
             // lblBurc
             // 
-            this.lblBurc.AutoSize = true;
-            this.lblBurc.Location = new System.Drawing.Point(310, 186);
+            resources.ApplyResources(this.lblBurc, "lblBurc");
             this.lblBurc.Name = "lblBurc";
-            this.lblBurc.Size = new System.Drawing.Size(58, 13);
-            this.lblBurc.TabIndex = 2;
-            this.lblBurc.Text = "Burcunuz :";
-            // 
-            // lblYasHesap
-            // 
-            this.lblYasHesap.AutoSize = true;
-            this.lblYasHesap.Location = new System.Drawing.Point(386, 141);
-            this.lblYasHesap.Name = "lblYasHesap";
-            this.lblYasHesap.Size = new System.Drawing.Size(0, 13);
-            this.lblYasHesap.TabIndex = 3;
-            // 
-            // lblYasananGunHesap
-            // 
-            this.lblYasananGunHesap.AutoSize = true;
-            this.lblYasananGunHesap.Location = new System.Drawing.Point(386, 164);
-            this.lblYasananGunHesap.Name = "lblYasananGunHesap";
-            this.lblYasananGunHesap.Size = new System.Drawing.Size(0, 13);
-            this.lblYasananGunHesap.TabIndex = 3;
-            // 
-            // lblBurcHesap
-            // 
-            this.lblBurcHesap.AutoSize = true;
-            this.lblBurcHesap.Location = new System.Drawing.Point(386, 186);
-            this.lblBurcHesap.Name = "lblBurcHesap";
-            this.lblBurcHesap.Size = new System.Drawing.Size(0, 13);
-            this.lblBurcHesap.TabIndex = 3;
             // 
             // btnHesapla
             // 
-            this.btnHesapla.Location = new System.Drawing.Point(314, 92);
+            resources.ApplyResources(this.btnHesapla, "btnHesapla");
             this.btnHesapla.Name = "btnHesapla";
-            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
-            this.btnHesapla.TabIndex = 4;
-            this.btnHesapla.Text = "Hesapla";
             this.btnHesapla.UseVisualStyleBackColor = true;
             this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnHesapla);
-            this.Controls.Add(this.lblBurcHesap);
-            this.Controls.Add(this.lblYasananGunHesap);
-            this.Controls.Add(this.lblYasHesap);
             this.Controls.Add(this.lblBurc);
             this.Controls.Add(this.lblYasananGun);
             this.Controls.Add(this.lblYas);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtpDogumTarihi);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -146,9 +95,6 @@ namespace Ornek1
         private System.Windows.Forms.Label lblYas;
         private System.Windows.Forms.Label lblYasananGun;
         private System.Windows.Forms.Label lblBurc;
-        private System.Windows.Forms.Label lblYasHesap;
-        private System.Windows.Forms.Label lblYasananGunHesap;
-        private System.Windows.Forms.Label lblBurcHesap;
         private System.Windows.Forms.Button btnHesapla;
     }
 }
--- Ornek1/Form1.cs
+++ Ornek1/Form1.cs
@@ -24,7 +24,40 @@ namespace Ornek1
 
         private void btnHesapla_Click(object sender, EventArgs e)
         {
-           
+            //Görev 1 : Yaşını hesaplayınız.
+            DateTime dt = dtpDogumTarihi.Value;
+
+            int ay = Convert.ToInt32(dtpDogumTarihi.Value.Month);
+            int gun = Convert.ToInt32(dtpDogumTarihi.Value.Day);
+
+            TimeSpan gecenSure = DateTime.Now - dt;
+            lblYasananGun.Text= "Yaşadığınız Gün : " + ((int)(gecenSure.TotalDays)).ToString();
+            lblYas.Text = "Yaşınız :" + (DateTime.Now.Year - dt.Year).ToString();
+            if ((ay == 1 && gun > 20) || (ay == 2 && gun < 20))
+                lblBurc.Text = "Burcunuz : " + "Kova";
+            else if ((ay == 2 && gun > 18) || (ay == 3 && gun < 21))
+                lblBurc.Text = "Burcunuz : " + "Balık";
+            else if ((ay == 3 && gun > 20) || (ay == 4 && gun < 21))
+                lblBurc.Text = "Burcunuz : " + "Koç";
+            else if ((ay == 4 && gun > 20) || (ay == 5 && gun < 22))
+                lblBurc.Text = "Burcunuz : " + "Boğa";
+            else if ((ay == 5 && gun > 21) || (ay == 6 && gun < 22))
+                lblBurc.Text = "Burcunuz : " + "İkizler";
+            else if ((ay == 6 && gun > 21) || (ay == 7 && gun < 24))
+                lblBurc.Text = "Burcunuz : " + "Yengeç";
+            else if ((ay == 7 && gun > 23) || (ay == 8 && gun < 24))
+                lblBurc.Text = "Burcunuz : " + "Aslan";
+            else if ((ay == 8 && gun > 23) || (ay == 9 && gun < 24))
+                lblBurc.Text = "Burcunuz : " + "Başak";
+            else if ((ay == 9 && gun > 23) || (ay == 10 && gun < 24))
+                lblBurc.Text = "Burcunuz : " + "Terazi";
+            else if ((ay == 10 && gun > 23) || (ay == 11 && gun < 23)) 
+                lblBurc.Text = "Burcunuz : " + "Akrep";
+            else if ((ay == 11 && gun > 22) || (ay == 12 && gun < 23))
+                lblBurc.Text = "Burcunuz : " + "Yay";
+            else if ((ay == 12 && gun > 22) || (ay == 1 && gun < 21))
+                lblBurc.Text = "Burcunuz : " + "Oğlak";
+
         }
     }
 }
